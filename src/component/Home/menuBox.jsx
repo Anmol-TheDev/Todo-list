c1441f6dd15bc4d0bd18270b796896403e8b8a1b
matrixAJ27 <anmolsingh51200@gmai.com>
2024-09-01 21:50:41 +0530

    edit2.1

--- src/component/Home/menuBox.jsx
+++ src/component/Home/menuBox.jsx
@@ -1,93 +1,174 @@
 import {
-    Menubar,
-    MenubarContent,
-    MenubarItem,
-    MenubarMenu,
-    MenubarSubContent,
-    MenubarSubTrigger,
-    MenubarSub,
-    MenubarTrigger,
-  } from "@/components/ui/menubar"
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarSub,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { MdSubdirectoryArrowLeft } from "react-icons/md";
 import { HiDotsVertical } from "react-icons/hi";
 import { FaRegStar } from "react-icons/fa";
 import { getAuth } from "firebase/auth";
-import { update,getDatabase,ref } from "firebase/database";
+import { update, getDatabase, ref,push,set } from "firebase/database";
 import app from "@/firebase/firebaseConfigure";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
-
-export function MenuBar ({props,changeClr,hndlStar}) {
-    const auth = getAuth()
-    const db = getDatabase(app) 
-    
-    function handlePiritoryInDb (argument,color) {
-        const newRef = ref(db, `${auth?.currentUser?.uid}/${props.id}`);
-            update(newRef, {
-              todoTitle:props.todoTitle,
-              completed: props.completed,
-              id: props.id,
-              time:props.time,
-              menu: {
-                priority:argument,
-                priority_bg: color,
-              },
-              star:props.star
-            }).then(()=>{
-             changeClr(props.id,color,argument)
-            }).catch((error)=>{
-                console.log(error)
-            })
-              
-    }
-
-  const   handleStar = ( ) =>{
-      const Ref = ref(db, `${auth?.currentUser?.uid}/${props.id}`);
-      if(props.star==false){
-        update(Ref,{
-          todoTitle:props.todoTitle,
-          completed: props.completed,
-          id: props.id,
-          time:props.time,
-          menu: {
-            priority:props.menu.priority,
-            priority_bg: props.menu.priority_bg,
-          },
-          star:true,
-        }).then(()=>hndlStar(true,props.id))
-      } else{
-        update(Ref,{
-          todoTitle:props.todoTitle,
-          completed: props.completed,
-          id: props.id,
-          time:props.time,
-          menu: {
-            priority:props.menu.priority,
-            priority_bg: props.menu.priority_bg,
-          },
-          star:false,
-        }).then(()=>hndlStar(false,props.id))
-      }
+export function MenuBar({ props, changeClr, hndlStar }) {
+  const auth = getAuth();
+  const db = getDatabase(app);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [textRef,setTextRef] = useState()
+  var Ref = ref(db, `${auth?.currentUser?.uid}/${props.id}`);
+  function handlePiritoryInDb(argument, color) {
+    update(newRef, {
+      todoTitle: props.todoTitle,
+      completed: props.completed,
+      id: props.id,
+      time: props.time,
+      menu: {
+        priority: argument,
+        priority_bg: color,
+      },
+      star: props.star,
+    })
+      .then(() => {
+        changeClr(props.id, color, argument);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-
-
-    return (
-        <Menubar className=" float-right border-0" >
-            <MenubarMenu>
-                <MenubarTrigger><HiDotsVertical/></MenubarTrigger>
-            <MenubarContent>
+  const handleStar = () => {
+    if (props.star == false) {
+      update(Ref, {
+        todoTitle: props.todoTitle,
+        completed: props.completed,
+        id: props.id,
+        time: props.time,
+        menu: {
+          priority: props.menu.priority,
+          priority_bg: props.menu.priority_bg,
+        },
+        star: true,
+      }).then(() => hndlStar(true, props.id));
+    } else {
+      update(Ref, {
+        todoTitle: props.todoTitle,
+        completed: props.completed,
+        id: props.id,
+        time: props.time,
+        menu: {
+          priority: props.menu.priority,
+          priority_bg: props.menu.priority_bg,
+        },
+        star: false,
+      }).then(() => hndlStar(false, props.id));
+    }
+  }
+  const handleSubTodo = ( ) => {
+    const subTodoRef = push(ref(db, `${auth?.currentUser?.uid}/${props.id}/subtodo`));
+        if(textRef==""){
+         
+        } else {
+          console.log("else")
+          set(subTodoRef, {
+                title:textRef,
+                completed:false,
+                id:subTodoRef.key
+            
+          }).then(()=>console.log("subtodo"))
+        }
+    }
+  return (
+    <>
+      <Menubar className=" float-right border-0">
+        <MenubarMenu>
+          <MenubarTrigger>
+            <HiDotsVertical />
+          </MenubarTrigger>
+          <MenubarContent>
             <MenubarSub>
-            <MenubarItem onClick={()=>{handleStar(props)}} > Star<FaRegStar/></MenubarItem>
-          </MenubarSub>
+              <MenubarItem
+                onClick={() => {
+                  handleStar(props);
+                }}
+              >
+                <p className="pr-3">Star</p>
+                <FaRegStar />
+              </MenubarItem>
+            </MenubarSub>
             <MenubarSub>
-            <MenubarSubTrigger>Piritory</MenubarSubTrigger>
-            <MenubarSubContent>
-              <MenubarItem onClick={()=>{handlePiritoryInDb("High","bg-red-200" )}} className=" focus:bg-red-300 ">High</MenubarItem>
-              <MenubarItem onClick={()=>{handlePiritoryInDb("medium","bg-yellow-200")}} className="focus:bg-yellow-300">Medium</MenubarItem>
-              <MenubarItem onClick={()=>{handlePiritoryInDb("low","bg-green-200")}} className="focus:bg-green-300">Low</MenubarItem>
-            </MenubarSubContent>
-          </MenubarSub>
+              <MenubarItem
+                onClick={() => {
+                  isModalOpen == true
+                    ? setIsModalOpen(false)
+                    : setIsModalOpen(true);
+                }}
+              >
+                 <MdSubdirectoryArrowLeft />
+              </MenubarItem>
+            </MenubarSub>
+            <MenubarSub>
+              <MenubarSubTrigger>Piritory</MenubarSubTrigger>
+              <MenubarSubContent>
+                <MenubarItem
+                  onClick={() => {
+                    handlePiritoryInDb("High", "bg-red-200");
+                  }}
+                  className=" focus:bg-red-300 "
+                >
+                  High
+                </MenubarItem>
+                <MenubarItem
+                  onClick={() => {
+                    handlePiritoryInDb("medium", "bg-yellow-200");
+                  }}
+                  className="focus:bg-yellow-300"
+                >
+                  Medium
+                </MenubarItem>
+                <MenubarItem
+                  onClick={() => {
+                    handlePiritoryInDb("low", "bg-green-200");
+                  }}
+                  className="focus:bg-green-300"
+                >
+                  Low
+                </MenubarItem>
+              </MenubarSubContent>
+            </MenubarSub>
           </MenubarContent>
-            </MenubarMenu>
-        </Menubar>
-    )
+        </MenubarMenu>
+      </Menubar>
+
+      <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DialogContent className="p-4">
+          <DialogHeader>
+            <DialogTitle>Enter You Todo Title</DialogTitle>
+            <DialogDescription className="p-2">
+              <Input placeholder="Enter your ToDo Title" onChange={(e)=>setTextRef(e.target.value)} />
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button onClick={()=>{handleSubTodo(),setIsModalOpen(false)}} >Submit</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 }
--- src/component/Home/todoPage.jsx
+++ src/component/Home/todoPage.jsx
@@ -1,4 +1,14 @@
-import {getDatabase,ref,set,get,push,onValue,remove,update,serverTimestamp} from "firebase/database";
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  push,
+  onValue,
+  remove,
+  update,
+  serverTimestamp,
+} from "firebase/database";
 import { getAuth } from "firebase/auth";
 import app from "../../firebase/firebaseConfigure";
 import { createContext, useEffect, useState } from "react";
@@ -6,67 +16,70 @@ import { onAuthStateChanged } from "firebase/auth";
 import { Button } from "@/components/ui/button";
 import UserProfile from "../profile";
 import { Input } from "@/components/ui/input";
-import {MenuBar} from "./menuBox"
+import { MenuBar } from "./menuBox";
 import { TiStarFullOutline } from "react-icons/ti";
-
 
 export const ProfileContext = createContext();
 
 function TodoPage() {
   const db = getDatabase(app);
-  const [userId,setUserId]= useState()
+  const [userId, setUserId] = useState();
   const auth = getAuth();
   const [title, setTitle] = useState("");
   const [todos, setTodo] = useState([]);
-  let [todoCount,setTodoCount] = useState(0);
-  let [completedTodo,setCompletedTodo]= useState(0);
-useEffect(()=>{
-  onAuthStateChanged(auth,(user)=>{
-    if(user){
-        setUserId(user.uid)
-    }
-})
-},[])
-
-useEffect(() => { //fetching stored value from database 
-
-    if(!userId) return
-    const dataref = ref(db,userId );
-    get(dataref).then((snapshot) => {
-      if (snapshot.exists()) {
-        const data = snapshot?.val() || {};
-        setTodo(Object.values(data));
-      } else {
-        console.log("No data available");
+  let [todoCount, setTodoCount] = useState(0);
+  let [completedTodo, setCompletedTodo] = useState(0);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserId(user.uid);
       }
-    }).catch((error) => {
-      console.error(error);
     });
-  },[userId]);
+  }, []);
 
-  useEffect(()=>{
-  setTodoCount(0);
-  setCompletedTodo(0);
-    if(todos){
-      setTodoCount(todos.length);
-      todos.forEach((e)=>{
-        if(e?.completed==true){
-          setCompletedTodo(++completedTodo);
+  useEffect(() => {
+    //fetching stored value from database
+
+    if (!userId) return;
+    const dataref = ref(db, userId);
+    get(dataref)
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot?.val() || {};
+          setTodo(Object.values(data));
+        } else {
+          console.log("No data available");
         }
       })
-    }
-  },[todos])
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [userId]);
 
-  const  HandleSubmit = async(e) => {   //setting data on submit
+  useEffect(() => {
+    setTodoCount(0);
+    setCompletedTodo(0);
+    if (todos) {
+      setTodoCount(todos.length);
+      todos.forEach((e) => {
+        if (e?.completed == true) {
+          setCompletedTodo(++completedTodo);
+        }
+      });
+    }
+  }, [todos]);
+
+  const HandleSubmit = async (e) => {
+    //setting data on submit
     e?.preventDefault();
-    let dateRef = new Date()
-    let temp= dateRef.toLocaleString('en-US',{
-      year:'numeric', 
-      month:'long',
-      week:'long',
-      hour: '2-digit',
-      minute: '2-digit',
-    })
+    let dateRef = new Date();
+    let temp = dateRef.toLocaleString("en-US", {
+      year: "numeric",
+      month: "long",
+      week: "long",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
     if (title.trim() === "") return; // Prevent submitting empty todos
     const newTodoRef = push(ref(db, auth?.currentUser?.uid));
     set(newTodoRef, {
@@ -74,122 +87,165 @@ useEffect(() => { //fetching stored value from database
       completed: false,
       id: newTodoRef.key,
       time: temp,
-      star:false,
-      menu:{
-        priority:false,
-        priority_bg: "bg-slate-400"
-      }
+      star: false,
+      menu: {
+        priority: false,
+        priority_bg: "bg-slate-400",
+      },
     }).catch((error) => {
       console.error("Error adding new todo:", error);
-    })
-    handleDbChange()
+    });
+    handleDbChange();
     setTitle("");
   };
- // for Readign Updated data from DB
-  const handleDbChange = () => {  //refrasing the db data on any change
-    const dataref = ref(db,userId)
-    setTodo([])
+  // for Readign Updated data from DB
+  const handleDbChange = () => {
+    //refrasing the db data on any change
+    const dataref = ref(db, userId);
+    setTodo([]);
     onValue(dataref, (snapshot) => {
       const data = snapshot.val();
       setTodo(Object.values(data));
     });
-  }
-  const handleDelete = (argument) => {  //function for deleting todo
-    const dataref = ref(db,`${userId}/${argument}`)
-    remove(dataref).then(()=>{
-      handleDbChange()
-    })
-  }
-  const handleCheck = (argument,e) => {  //checkbox in todo
+  };
+  const handleDelete = (argument) => {
+    //function for deleting todo
+    const dataref = ref(db, `${userId}/${argument}`);
+    remove(dataref).then(() => {
+      handleDbChange();
+    });
+  };
+  const handleCheck = (argument, e) => {
+    //checkbox in todo
     const newRef = ref(db, `${auth?.currentUser?.uid}/${argument.id}`);
-if(e?.target.checked){
-    const updatedTodo = todos.map((todo)=>(
-      argument.id==todo.id ? {...todo,completed:true} : todo
-    ))
-    setTodo(updatedTodo)
-    update(newRef, {
-      todoTitle:argument.todoTitle,
-      completed: true,
-      id: newRef.key,
-      time:argument.time,
-      star:argument.star,
-      menu:{
-        priority:argument.menu.priority,
-        priority_bg:argument.menu.priority_bg
-      }
-    })
+    if (e?.target.checked) {
+      const updatedTodo = todos.map((todo) =>
+        argument.id == todo.id ? { ...todo, completed: true } : todo
+      );
+      setTodo(updatedTodo);
+      update(newRef, {
+        todoTitle: argument.todoTitle,
+        completed: true,
+        id: newRef.key,
+        time: argument.time,
+        star: argument.star,
+        menu: {
+          priority: argument.menu.priority,
+          priority_bg: argument.menu.priority_bg,
+        },
+      });
+    } else {
+      const updatedTodo = todos.map((todo) =>
+        argument.id == todo.id ? { ...todo, completed: false } : todo
+      );
+      setTodo(updatedTodo);
+      update(newRef, {
+        todoTitle: argument.todoTitle,
+        completed: false,
+        id: newRef.key,
+        time: argument.time,
+        star: argument.star,
+        menu: {
+          priority: argument.menu.priority,
+          priority_bg: argument.menu.priority_bg,
+        },
+      });
+    }
+    setCompletedTodo(0);
+  };
 
-  } else {
-    const updatedTodo = todos.map((todo)=>(
-      argument.id==todo.id ? {...todo,completed:false} : todo
-    ))
-    setTodo(updatedTodo)
-    update(newRef, {
-      todoTitle:argument.todoTitle,
-      completed: false,
-      id: newRef.key,
-      time:argument.time,
-      star:argument.star,
-      menu:{
-        priority:argument.menu.priority,
-        priority_bg:argument.menu.priority_bg
-      }
-    })   
+  function handleMenuChange(Id, color, priority) {
+    // for changing Todo bg according to priority locally
+    const updatedTodo = todos.map((todo) =>
+      todo.id === Id
+        ? {
+            ...todo,
+            menu: { ...todo.menu, priority: priority, priority_bg: color },
+          }
+        : todo
+    );
+    setTodo(updatedTodo);
   }
-  setCompletedTodo(0)
-  }
-
-  function handleMenuChange (Id,color,priority) {  // for changing Todo bg according to priority locally
-   const updatedTodo = todos.map((todo)=>(
-      todo.id===Id ?  {...todo,menu:{...todo.menu,priority: priority , priority_bg: color}} : todo
-   ))
-      setTodo(updatedTodo)
-  }
-  function handleStar (status,Id) {
-      const updatedTodo = todos.map((todo)=>(
-        todo.id==Id ? {...todo,star:status} : todo
-      ))
-      setTodo(updatedTodo)
+  function handleStar(status, Id) {
+    const updatedTodo = todos.map((todo) =>
+      todo.id == Id ? { ...todo, star: status } : todo
+    );
+    setTodo(updatedTodo);
   }
 
   return (
-    <ProfileContext.Provider value={{todoCount,completedTodo}}>
-    <div className="p-8 ">
-      <div className="flex flex-col gap-4 ">
-        <div className="flex justify-between ">
-        <h1 className="text-5xl self-center">TO DO </h1>
-          < UserProfile />
-        </div>
-        <div className="flex justify-center">
-          <form onSubmit={HandleSubmit} className="flex gap-7">
-            <Input className="border-2 text-xl px-2  rounded-md"
-              type="text"
-              value={title}
-              onChange={(e) => {
-                setTitle(e?.target.value);
-              }}
-              required
-            />
-            <Button>Add todo </Button> 
-          </form>
-        </div>
-        <div className="flex flex-wrap gap-4">
-          {todos?.map((todo,i) => (
-            <div className={`border-2 p-4 px-8flex flex-col w-full rounded-md shadow-lg ${todo?.menu ? todo.menu.priority_bg : "bg-slate-400" } `} key={todo?.id||i}  >
-              <MenuBar props={todo} changeClr={handleMenuChange} hndlStar={handleStar} />
-              <div className="flex gap-3">
-              <input type="checkbox" checked={todo?.completed} onChange={(e)=>handleCheck(todo,e)}/>
-              { todo?.star==true ?  <TiStarFullOutline /> : "" }
-              </div>
-              <h1 className={`${todo?.completed ? "line-through" : ""} text-3xl`}>{todo?.todoTitle}</h1>
-              <p>{todo?.completed ? "Completed" : "Incomplete"}</p>
-              <p>{todo?.time}</p>
-              <button className="text-red-400 float-right pr-4" onClick={()=>handleDelete(todo?.id)}><i className="fa-solid fa-trash"></i></button>
-            </div>
-          ))}
+    <ProfileContext.Provider value={{ todoCount, completedTodo }}>
+      <div className="p-8 ">
+        <div className="flex flex-col gap-4 ">
+          <div className="flex justify-between ">
+            <h1 className="text-5xl self-center">TO DO </h1>
+            <UserProfile />
+          </div>
+          <div className="flex justify-center">
+            <form onSubmit={HandleSubmit} className="flex gap-7">
+              <Input
+                className="border-2 text-xl px-2  rounded-md"
+                type="text"
+                value={title}
+                onChange={(e) => {
+                  setTitle(e?.target.value);
+                }}
+                required
+              />
+              <Button>Add todo </Button>
+            </form>
+          </div>
+          <div className="flex flex-wrap gap-4">
+            {todos?.map((todo, i) => {
+                const subtodo = Object?.values(todo?.subtodo?? {})
+              return (
+              <>
+                <div
+                  className={`border-2 p-4 px-8flex flex-col w-full rounded-md shadow-lg ${
+                    todo?.menu ? todo.menu.priority_bg : "bg-slate-400"
+                  } `}
+                  key={todo?.id || i}
+                >
+                  <MenuBar
+                    props={todo}
+                    changeClr={handleMenuChange}
+                    hndlStar={handleStar}
+                  />
+                  <div className="flex gap-3">
+                    <input
+                      type="checkbox"
+                      checked={todo?.completed}
+                      onChange={(e) => handleCheck(todo, e)}
+                    />
+                    {todo?.star == true ? <TiStarFullOutline /> : ""}
+                  </div>
+                  <h1
+                    className={`${
+                      todo?.completed ? "line-through" : ""
+                    } text-3xl`}
+                  >
+                    {todo?.todoTitle}
+                  </h1>
+                  <p>{todo?.completed ? "Completed" : "Incomplete"}</p>
+                  <p>{todo?.time}</p>
+                  <button
+                    className="text-red-400 float-right pr-4"
+                    onClick={() => handleDelete(todo?.id)}
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </div>
+                {subtodo?.map((subtodo,i)=>(
+                  <div key={i}  >
+                    <input type="checkbox" checked={subtodo.check} />
+                    <h1>{subtodo.title}</h1>
+                  </div>
+            ))}
+              </>)
+            })}
+          </div>
         </div>
       </div>
-    </div>
     </ProfileContext.Provider>
   );
 }
